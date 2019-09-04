57192c3e9bc4ffd2bd472f01bf41dba05ca35be3
Jessica <jessicadao97@gmail.com>
2019-09-03 23:30:56 -0400

    responsive and phone menu. styling...

--- src/app/all-tasks/all-tasks.component.ts
+++ src/app/all-tasks/all-tasks.component.ts
@@ -15,22 +15,19 @@ export class AllTasksComponent implements OnInit {
   allDatesFormat = new Map()
   async ngOnInit() {
     this.allTasks = await this.taskDataService.fetchAllTasks();
-    console.log("all tasks" +await this.allTasks.length)
+    // console.log("all tasks" +await this.allTasks.length)
     await this.getDates()
   }
   getDates() {
     for (let task of this.allTasks) {
       if (task.attrs.dateCreated !== '') {
         if (!this.allDates.has(task.attrs.dateCreated) ) {
-          // console.log("type "+JSON.stringify(task.attrs.dateCreated) )
           this.allDates.set(task.attrs.dateCreated,[])
           this.allDatesFormat.set(task.attrs.dateCreated, moment(task.attrs.dateCreated,'YYYY/MM/DD').format('MMMM-DD-YYYY'))
         }
         var tasks = this.allDates.get(task.attrs.dateCreated)
         tasks.push(task.attrs.name)
         this.allDates.set(task.attrs.dateCreated,tasks)
-        console.log("push "+JSON.stringify(this.allDatesFormat.get(task.attrs.dateCreated) ))
-
       }
       
     }
